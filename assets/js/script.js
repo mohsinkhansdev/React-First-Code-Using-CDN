4368386972f76bd52f58b97937d0701b2602cc2e
Muhammad Mohsin Khan <77793691+mohsinkhansdev@users.noreply.github.com>
2022-02-17 22:22:18 +0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,81 @@
+// React First App Using CDN
+//JSX
+
+/* function page(){
+    return(
+    <div>
+        <header>
+            <nav>
+                 <img src="assets/img/react-logo.png" style="width: 15%;"/>
+            </nav>
+        </header>
+        
+
+        <h1>Why I'm Exciting to learn about React </h1>
+            <ol>
+                <li>Its Popular Javascript Library</li>
+                <li>With 100k github starts</li>
+                <li>Its a high professional skill with market demanding </li>
+                <li>I'm more likely to get a job as a developer once grip the concepts in react </li>
+            </ol>
+
+        <footer>
+            <small>Ⓒ 2021 <span> MMK </span> developer. All rights reserved</small>
+        </footer>    
+    </div>
+    )
+    
+}
+
+ReactDOM.render(
+    <page /> 
+    ,
+    document.getElementById("root")
+) */
+
+/* const nav = (
+    <nav>
+        <h1>Navbar</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    nav,
+    document.getElementById("root")
+) */
+
+/* const page = (
+    <div>
+        <h1 className = "header">This is JSX</h1>
+        <p>This is a Paragraph</p>
+    </div>
+);
+console.log(page)
+
+
+ ReactDOM.render(
+    page, 
+    document.getElementById("root") 
+    ); */
+/*
+ReactDOM.render (<h1>Hello, React!</h1>, 
+    document.getElementById("root")) */
+
+
+
+/* function MainContent() {
+    return (
+        <h1>I'm Learning React!</h1>
+    )
+}
+ReactDOM.render (<h1>Hello, React!</h1>, document.getElementById())
+ReactDOM.render( 
+<div>
+    <MainContent/> 
+</div>
+    ,document.getElementById("root")); */
